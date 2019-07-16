ffa221b2269daa9891dba6e4ac336cc2e9117750
Stephen Turley <stephen.turley@protonmail.com>
2019-07-15 22:48:39 -0400

    added gutters singles and spares

--- bowling.js
+++ bowling.js
@@ -1,5 +1,29 @@
 let score = (line) => {
-  return 0;
+  let frames = line.split(' ');
+  let score = 0;
+
+  frames.forEach((frame)=>{
+    score += valueOf(frame[0]);
+    score += valueOf(frame[1]);
+  });
+  return score;
 }
+
+let valueOf = (roll) => {
+  switch(roll) {
+    case '-': {
+      return 0;
+      break;
+    }
+    case '/': {
+      return 10;
+      break;
+    }
+    default: {
+      return parseInt(roll);
+      break;
+    }
+  }
+};
 
 export default score;
--- bowling.test.js
+++ bowling.test.js
@@ -1,6 +1,18 @@
 import score from './bowling.js';
 
-test('should sore a zero', () => {
-  let result = score('- - - - - - - - - -');
-  expect(result).toBe(0);
+describe('score', () => {
+  test('should sore a zero', () => {
+    let result = score('-- -- -- -- -- -- -- -- -- --');
+    expect(result).toBe(0);
+  });
+
+  test('should score 9s', () => {
+    let result = score('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-');
+    expect(result).toBe(90);
+  });
+
+  test('should score spares', () => {
+    let result = score('1/ 1/ 1/ 1/ 1/ 1/ 1/ 1/ 1/ 1/-');
+    expect(result).toBe(110);
+  });
 });
